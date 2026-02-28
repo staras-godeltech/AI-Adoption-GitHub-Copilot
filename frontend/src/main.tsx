2b21ebd7ceefedda6c97d2ee456d45c627bb43f9
Siarhei Taras <siarhei.taras@ensek.co.uk>
2026-02-28 16:10:30 +0100

    installed react-big-calendar

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
